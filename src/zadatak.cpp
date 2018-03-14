6d3fb5e551dd13155632994e454808cdc80712df
Mladen Jurak <jurak@pred-pr2.studenti.math.hr>
2018-03-14 15:24:22 +0100

    Gotov.

--- src/zadatak.cpp
+++ src/zadatak.cpp
@@ -1,10 +1,9 @@
 #include "zadatak.h"
 // Ubaciti potrebne include datoteke
 
-#include <cmath>
 
 double zadatak(double radijus){
   // Implementacija zadatka
-   return 2*radijus*M_PI;
+
 }
 
--- src/zadatak.h
+++ src/zadatak.h
@@ -1,7 +1,6 @@
 #ifndef __ZADATAK_IS_INCLUDED__             
 #define __ZADATAK_IS_INCLUDED__             
 
-#include <cmath>
 
 double zadatak(double radijus);
 
